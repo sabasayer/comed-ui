a49168b8b2640ac65f9feee3247e57a1526c864a
salihbaki <salihbakisayer@hotmail.com.tr>
2020-08-25 18:53:48 +0300

    inputs

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # comed-ui
+
+[IBM Carbon Design](https://github.com/carbon-design-system/carbon) used as foundation 
+
+To see examples run :
+
+`yarn serve`
+
+Then go to `localhost:8080/index.html`
--- src/site-map.js
+++ src/site-map.js
@@ -1,4 +1,10 @@
 export const siteMap = {
   Buttons: "components/button/button.html",
   "Side Nav": "components/side-nav/side-nav.html",
+  Checkbox: "components/form/checkbox/checkbox.html",
+  "Text input": "components/form/text-input/text-input.html",
+  "Text area": "components/form/text-area/text-area.html",
+  Select: "components/form/select/select.html",
+  Toggle: "components/form/toggle/toggle.html",
+  "Page Drawer": "components/page-drawer/page-drawer.html",
 };
